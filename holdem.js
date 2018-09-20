8c37e22a91bf5c750b32dbfcfefce981b59278b0
Gregory7773 <36812127+Gregory7773@users.noreply.github.com>
2018-09-20 21:23:52 +0200

    Fixed alghorithm in all-in case

--- holdem.js
+++ holdem.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 
+  var i = 3;
+  var current_call = 65;
+  var back_call;
+  var raise_index = 3;
+  var counter = 3;
+  var iteration = 0;
   var initPositions = (function(){
 
     function initPosCards(n){
@@ -69,31 +75,114 @@ $(document).ready(function(){
 
     return {
       initialPositions: function(n){
-        initPosCards(n);
         initPosPlayers(n);
         initPosCoins(n);
+      },
+      initCardsPositions: function(n){
+        initPosCards(n);
       }
     }
 
   })();
 
+  var UserInterface = (function(){
 
-  var Cards = (function(){
+
+    var animateCards = function(){
+      $(".card").each(function(index){
+        $(this).css({transitionDelay:150*index+"ms"});
+          })
+    };
+
+
+    return{
+      showPlayerCards: function(players,n){
+        var insertCardHTML = '<img class = "card" src = "images/cards/'+players[0].Card1x+'-'+players[0].Card1y+'.png">'
+        $("#player").before(insertCardHTML);
+
+        for(var i = 0 ; i< (n-2);i++){
+          $("#player").before('<img class = "card" src = "images/cards/card back.png">')
+        }
+        insertCardHTML = '<img class = "card" src = "images/cards/'+players[0].Card2x+'-'+players[0].Card2y+'.png">'
+        $("#player").before(insertCardHTML);
+
+        for(var i = 0 ; i< (n-2);i++){
+          $("#player").before('<img class = "card" src = "images/cards/card back.png">')
+        }
+        animateCards();
+      },
+      toggleButtons: function(players){
+        if(current_call == -1){
+          $("#check").toggle();
+          $("#raise").toggle();
+        }
+        else if(current_call >= players[0].money){
+          $("#fold").toggle();
+          $("#all_in").toggle();
+        }
+        else{
+          $("#fold").toggle();
+          $("#call").toggle();
+          $("#raise").toggle();
+        }
+      },
+      flop: function(cards,iteration){
+        if(iteration == 1){
+          $(".flop-river-turn").append('<img class = "common-card" src = "images/cards/'+cards[0]+'.png">');
+          $(".flop-river-turn").append('<img class = "common-card" src = "images/cards/'+cards[1]+'.png">');
+          $(".flop-river-turn").append('<img class = "common-card" src = "images/cards/'+cards[2]+'.png">');
+        }
+        else if(iteration == 2){
+          $(".flop-river-turn").append('<img class = "common-card" src = "images/cards/'+cards[3]+'.png">')
+        }
+        else{
+          $(".flop-river-turn").append('<img class = "common-card" src = "images/cards/'+cards[4]+'.png">')
+        }
+      }
+
+    }
+  })();
+
+
+  var Cards = (function(userInt){
 
     var x,y;
     var time_out_variable = 1;
-    var player = function (Card1x,Card1y,Card2x,Card2y,money,fold,user_call){
+    var player = function (Card1x,Card1y,Card2x,Card2y,money,fold,user_call,all_in){
       this.Card1x = Card1x;
       this.Card1y = Card1y;
-      this.Card2x = Card2x
-      this.Card2y = Card2y
+      this.Card2x = Card2x;
+      this.Card2y = Card2y;
       this.money = money;
       this.fold = fold;
       this.user_call = user_call;
+      this.all_in = all_in;
     };
 
     var Players = [];
     var RandomCards = [];
+    var flop_river_turn_cards = [];
+
+
+    function flopRiverTurnDraw(){
+      var one,two,three,four,joined,is_repeat;
+      for(var i =0; i<5;i++){
+        do {
+          is_repeat = false;
+          one = Math.round((Math.random()*12)+2);
+          two = Math.round((Math.random()*3)+1);
+          joined = one+"-"+two;
+          RandomCards.forEach(function(element){
+            if(joined == element){is_repeat=true;}
+          })
+        } while (is_repeat);
+        flop_river_turn_cards.push(joined);
+        RandomCards.push(joined);
+      }
+
+    };
+
+
 
     player.prototype.drawCards = function(){
       var one,two,three,four,joined,is_repeat;
@@ -101,7 +190,7 @@ $(document).ready(function(){
         is_repeat = false;
         one = Math.round((Math.random()*12)+2);
         two = Math.round((Math.random()*3)+1);
-        joined = one+"."+two;
+        joined = one+"-"+two;
         RandomCards.forEach(function(element){
           if(joined == element){is_repeat=true;}
         })
@@ -114,7 +203,7 @@ $(document).ready(function(){
         is_repeat = false;
         three = Math.round((Math.random()*12)+2);
         four = Math.round((Math.random()*3)+1);
-        joined = three+"."+four;
+        joined = three+"-"+four;
         RandomCards.forEach(function(element){
           if(joined == element){is_repeat=true;}
         })
@@ -151,7 +240,7 @@ $(document).ready(function(){
       addCoins(coinsArray,id);
     }
 
-    function playerChoice(element,current_call,id){
+    function playerChoice(element,current_call,id,check){
       var back_call,separate_call;
       if((element.Card1x == element.Card2x)&&(element.Card1x > 7)){
         back_call = element.money;
@@ -160,6 +249,7 @@ $(document).ready(function(){
         console.log(Players.indexOf(element)+" "+back_call);
         howManyCoins(separate_call,id);
         element.user_call = current_call;
+        element.all_in = true;
         return back_call;
       }
       if(element.Card1x >9 && element.Card2x > 9 && current_call < 0.2*element.money){
@@ -181,21 +271,10 @@ $(document).ready(function(){
         element.user_call = current_call;
         return back_call;
       }
-
       element.fold = true;
       return current_call;
     }
 
-    function mainPlayerChoice(element,current_call,id){
-      var back_call,separate_call;
-      back_call = current_call;
-      separate_call = back_call - element.user_call;
-      element.money -= separate_call;
-      console.log(Players.indexOf(element)+" "+back_call);
-      howManyCoins(separate_call,id);
-      element.user_call = current_call;
-      return back_call;
-    }
 
     function addCoins(coinsArray,id){
       var coin_value;
@@ -244,7 +323,61 @@ $(document).ready(function(){
       return Math.round(Math.random()*80);
     }
 
+    function anyPlayersLeft(){
+      var players_left = 0;
+      Players.forEach(function(element){
+        if(element.fold != true){
+          players_left++;
+        }
+      });
+      if(players_left > 1){
+        return true;
+      }
+      else{
+        return false;
+      }
+    };
+
+    function playersLeftAfterAllIn(){
+      var players_left = 0;
+      Players.forEach(function(element){
+        if(element.fold == false && element.all_in == false){
+          players_left++;
+        }
+      });
+      if(players_left > 1){
+        return true;
+      }
+      else{
+        return false;
+      }
+    };
+    function cleanOutUserCall(){
+      Players.forEach(function(element){
+        element.user_call = 0;
+      })
+    };
+
     return{
+      mainPlayerChoice: function(){
+        if(Players[0].fold == true){
+          return;
+        }
+        else{
+          if(Players[0].all_in == true){
+            separate_call = Players[0].money;
+          }
+          else{
+            separate_call = back_call - Players[0].user_call;
+          }
+          Players[0].money -= separate_call;
+          console.log("Player 0: "+separate_call);
+          howManyCoins(separate_call,0);
+          Players[0].user_call = back_call;
+          return;
+        }
+      },
+
       createPlayers: function(n){
         for(var i = 0; i < n-1; i++){
           var Player = new player();
@@ -256,7 +389,11 @@ $(document).ready(function(){
           elem.drawCards();
           elem.fold = false;
           elem.user_call = 0;
+          elem.all_in = false;
         })
+      },
+      flopRiverTurn: function(){
+        flopRiverTurnDraw();
       },
       setMoneyToPlayers: function(){
         Players.forEach(function(element){
@@ -267,33 +404,75 @@ $(document).ready(function(){
         return Players;
       },
       queue: function(){
-        var i = 3;
-        var current_call = 65;
-        var back_call;
-        var raise_index = 3;
-        var counter = 3;
 
 
           (function TheLoop(){
-            setTimeout(function(){
-              if(back_call == current_call){
-                if( counter == raise_index){return;};
-              };
-
-              if(Players[i].fold != true){
-
-                if(i==0){
-                   back_call = mainPlayerChoice(Players[i],current_call,i);
+            if(back_call == current_call){
+              if(counter == raise_index){
+                if(iteration < 3){
+                  cleanOutUserCall();
+                  if(anyPlayersLeft()){
+                    current_call = 65;
+                  }
+                  else{
+                    return;
+                  }
+                  if(playersLeftAfterAllIn()){
+                    back_call = 0;
+                  }
+                  else{
+                    back_call = 65;
+                  }
+                  iteration++;
+                  raise_index = 1;
+                  counter = 1;
+                  i=1;
+                  console.log("gówno1");
+                  setTimeout(function(){
+                    userInt.flop(flop_river_turn_cards,iteration);
+                    console.log("gówno2");
+                    TheLoop();
+                  },1000);
+                  return;
                 }
                 else{
-                  back_call = playerChoice(Players[i],current_call,i);
+                  return;
+                }
+              };
+            };
+
+
+            if(Players[i].fold == true || Players[i].all_in == true)  {
+                counter +=1;
+                i += 1;
+                if(i< Players.length){
+                  TheLoop();
+                }
+                else{
+                  i = 0;
+                  counter = 0;
+                  if(Players[0].fold != true){
+                    userInt.toggleButtons(Players);
+                  }
+                  else{
+                    i = 1;
+                    counter = 1;
+                    TheLoop();
+                  }
+
+                }
+              }
+            else {
+              setTimeout(function(){
+
+                if(i>0){
+                  back_call = playerChoice(Players[i],current_call,i,back_call);
                 };
 
                 if(back_call>current_call){
                   current_call = back_call;
                   raise_index = counter;
                 };
-              };
               counter +=1;
               i += 1;
               if(i < Players.length)
@@ -302,50 +481,22 @@ $(document).ready(function(){
               }
               else{
                 i = 0;
-                counter = 0
-                TheLoop();
+                counter = 0;
+                if(Players[0].fold != true){
+                  userInt.toggleButtons(Players);
+                }
+                else{
+                  i = 1;
+                  counter = 1;
+                  TheLoop();
+                }
               }
-            },1000);
+            },100);
+            }
           })();
         }
-
-
     }
-  })();
-
-
-
-
-  var UserInterface = (function(){
-
-
-    var animateCards = function(){
-      $(".card").each(function(index){
-        $(this).css({transitionDelay:150*index+"ms"});
-          })
-    }
-
-
-    return{
-      showPlayerCards: function(players,n){
-        var insertCardHTML = '<img class = "card" src = "images/cards/'+players[0].Card1x+'-'+players[0].Card1y+'.png">'
-        $("#player").before(insertCardHTML);
-
-        for(var i = 0 ; i< (n-2);i++){
-          $("#player").before('<img class = "card" src = "images/cards/card back.png">')
-        }
-        insertCardHTML = '<img class = "card" src = "images/cards/'+players[0].Card2x+'-'+players[0].Card2y+'.png">'
-        $("#player").before(insertCardHTML);
-
-        for(var i = 0 ; i< (n-2);i++){
-          $("#player").before('<img class = "card" src = "images/cards/card back.png">')
-        }
-        animateCards();
-      }
-
-
-    }
-  })();
+  })(UserInterface);
 
 
 
@@ -353,20 +504,45 @@ $(document).ready(function(){
   var controller  = (function(initPos,Cards,UserInter){
 
     var n = 9;
+    var Players = Cards.players();
+    $("#check").on("click",function(){
+      back_call = -1;
+    });
 
-
-    Cards.createPlayers(n);
-    Cards.drawCard();
-    Cards.setMoneyToPlayers();
-    var players = Cards.players();
-    UserInter.showPlayerCards(players,n);
     $("#fold").on("click",function(){
-      initPos.initialPositions(n);
+      Players[0].fold = true;
+    });
+
+    $("#call").on("click",function(){
+      back_call = current_call;
+    });
+
+    $("#raise").on("click",function(){
+      back_call = $(".money-slider").attr("value");
+    });
+
+    $("#all_in").on("click",function(){
+      Players[0].all_in = true;
+    });
+
+    $(".button").on("click",function(){
+      UserInter.toggleButtons(Players);
+      Cards.mainPlayerChoice();
       Cards.queue();
     });
 
-
-
+    $(".deal-the-cards").on("click",function(){
+      $(".start-page").fadeOut(1000);
+      initPos.initCardsPositions(n);
+      setTimeout(function(){Cards.queue();},2500);
+    });
+    Cards.createPlayers(n);
+    Cards.drawCard();
+    Cards.flopRiverTurn();
+    Cards.setMoneyToPlayers();
+    UserInter.showPlayerCards(Players,n);
+    initPos.initialPositions(n);
+    $(".button").toggle();
 
 
   })(initPositions,Cards,UserInterface);
